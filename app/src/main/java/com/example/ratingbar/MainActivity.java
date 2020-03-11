34bf01a31d74b53345a2decb56d259c3bd7977a5
RAJLE-HOME22 <rajle22govinda@gmail.com>
2020-03-11 16:46:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ratingbar/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ratingbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar ratingbar;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ratingbar=(RatingBar)findViewById(R.id.ratingBar);
+        button=(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rating=String.valueOf(ratingbar.getRating());
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
